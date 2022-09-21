d8510d5f8ade513eb5d2e692dfa8c25e3a4fd2f8
djunpy <djunpy@gmail.com>
2022-09-22 02:08:18 +0300

    init

--- clothing_store/wsgi.py
+++ clothing_store/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'clothing_store.development')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'clothing_store.production')
 
 application = get_wsgi_application()
--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'clothing_store.development')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'clothing_store.production')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
